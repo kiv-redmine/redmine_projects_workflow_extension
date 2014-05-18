1055316954761bb75f390bb7760eaabf5adf6f4f
Strnadj <jan.strnadek@gmail.com>
2014-05-18 22:35:59 +0200

    TimeEntry - Cannot create entry when its greather than estimated time

--- vendor/plugins/redmine_project_workflow_extension/lib/patches/time_entry_patch.rb
+++ vendor/plugins/redmine_project_workflow_extension/lib/patches/time_entry_patch.rb
@@ -12,9 +12,15 @@ module RedmineProjectWorkflowExtension
           after_save :update_burndown_record
           after_destroy :update_burndown_record
 
+          # Validate estimated hours time
+          validate :issue_estimated_time
+
+          def issue_estimated_time
+            errors.add :hours, l(:error_issue_estimated_hours_exceeded) if (issue.estimated_hours) < (issue.spent_hours + hours)
+          end
+
           # Update sub_time records
           def update_burndown_record
-            Rails.logger.info "[BURNDOWN] Update \"#{project.identifier}\" sub time"
             BurndownRecord.update_project_sub_time(project, spent_on)
           end
         end
